b98c2e4c01fb7753846bda32075471c5424088c0
mileshkotadia987 <mileshkumar98@gmail.com>
2022-07-18 12:41:53 -0400

    implemented main class for utilizing the other classes for computing sinh(x).

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,48 @@
 package com.company;
 
+import java.util.Scanner;
+
+/**
+ * Main class
+ * @author MILESH
+ */
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        Scanner sc;
+        HyperbolicSineFunction sineFunction =new HyperbolicSineFunction();
+        System.out.println("---------- Sinh(x) Calculator -----------");
+        System.out.print("Enter the value of x: ");
+        sc=new Scanner(System.in);
+        if (sc.hasNextDouble()) {
+            double input = sc.nextDouble();
+            sineFunction.SinhComputation(input);
+        }
+        else{
+            System.out.println("The user input is not in proper format!!");
+//            sc.close();
+        }
+        while(true){
+            System.out.print("Do you want to continue(yes/no): ");
+            sc=new Scanner(System.in);
+            String userChoice = sc.next();
+            if (userChoice.equalsIgnoreCase("yes")){
+                System.out.print("Enter the value of x: ");
+                if (sc.hasNextDouble()) {
+                    double input = sc.nextDouble();
+                    sineFunction.SinhComputation(input);
+                }
+                else{
+                    System.out.println("The user input is not in proper format!!");
+                }
+            }
+            else if (userChoice.equalsIgnoreCase("no")){
+                System.out.println("Thank you!!");
+                System.exit(0);
+            }
+            else{
+                System.out.println("Please select a valid option!!");
+            }
+        }
     }
 }
