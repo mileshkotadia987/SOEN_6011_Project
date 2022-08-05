83a0f4965ccd397f50c122c3a46ac7cbff7aa491
mileshkotadia987 <mileshkumar98@gmail.com>
2022-08-05 15:56:27 -0400

    adding java comments

--- src/test/HyperbolicSineFunctionTest.java
+++ src/test/HyperbolicSineFunctionTest.java
@@ -1,5 +1,10 @@
 package test;
 
+
+/**
+ * This class validates the functions of HyperbolicSineFunction class
+ */
+
 import com.company.HyperbolicSineFunction;
 import org.junit.Assert;
 import org.junit.Before;
@@ -12,12 +17,19 @@ import static org.junit.Assert.*;
 public class HyperbolicSineFunctionTest {
     HyperbolicSineFunction sineFunction;
     DecimalFormat decimalFormat;
+
+    /**
+     * Initial Setup
+     */
     @Before
     public void setUpStreams() {
         sineFunction=new HyperbolicSineFunction();
         decimalFormat = new DecimalFormat(".##########");
     }
 
+    /**
+     * This function test the exponentialPower function
+     */
     @Test
     public void exponentialPower() {
         double num=1.0;
@@ -27,6 +39,9 @@ public class HyperbolicSineFunctionTest {
         Assert.assertEquals(actualOutput,expectedOutput,0.0);
     }
 
+    /**
+     * This function test the sinhComputation function
+     */
     @Test
     public void sinhComputation() {
         double actualOutput=1.0;
@@ -35,9 +50,11 @@ public class HyperbolicSineFunctionTest {
         Assert.assertNotEquals(actualOutput,expectedOutput);
     }
 
+    /**
+     * This function tests the functionEvaluation functiuonality
+     */
     @Test
     public void functionEvaluation() {
-//        DecimalFormat decimalFormat = new DecimalFormat(".###############");
         double input=1.0;
         double e1= sineFunction.exponentialPower(input);
         double e2= sineFunction.exponentialPower(-input);
--- src/test/ValidationTest.java
+++ src/test/ValidationTest.java
@@ -1,5 +1,9 @@
 package test;
 
+/**
+ * This class implementss the unit test cases for Validaion class
+ */
+
 import com.company.Validation;
 import org.junit.Assert;
 import org.junit.Test;
@@ -8,6 +12,9 @@ import static org.junit.Assert.*;
 
 public class ValidationTest {
 
+    /**
+     * This function tests the userInputValidation function
+     */
     @Test
     public void userInputValidation() {
         double lowerLimit = Double.MIN_VALUE;
@@ -16,6 +23,9 @@ public class ValidationTest {
         Assert.assertFalse(Validation.UserInputValidation(upperLimit));
     }
 
+    /**
+     * This function tests the outputValidation function
+     */
     @Test
     public void outputValidation() {
         double lowerLimit = Double.NEGATIVE_INFINITY;
